358d0a106717dbdce005df2dda104a8e4d68f74f
Archit Jain <77613361+architjain798@users.noreply.github.com>
2023-01-26 07:07:20 +0000

    Added the /calculations route

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World! from Github");
+});
+
+app.get("/calculations", (req, res) => {
+  let result = 0;
+  for (let index = 0; index < 100000000; index++) {
+    result += index;
+  }
+  return res.send({ processId: process.pid, result });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
